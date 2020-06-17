7d53b44bd88daa2228edf2f3e57416343e5679bf
ChunYS <48270425+ChunYS@users.noreply.github.com>
2020-06-18 04:25:17 +0900

    기본 프래그먼트 구현

--- app/src/main/java/org/techtown/databasefinalproject/SecondaryDescriptionActivity.java
+++ app/src/main/java/org/techtown/databasefinalproject/SecondaryDescriptionActivity.java
@@ -1,13 +1,21 @@
 package org.techtown.databasefinalproject;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SecondaryDescriptionActivity extends AppCompatActivity {
 
+    CreatureFragment  creatureFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary_description);
+
+        creatureFragment = new CreatureFragment();
+
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.activity_secondary_frameLayout_1, creatureFragment ).commit();
     }
 }
